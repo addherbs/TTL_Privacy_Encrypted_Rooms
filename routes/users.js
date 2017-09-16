b3e5c8def7fd7a7989309117cfbaf8ea3f5cf5d3
Aditya Pandey <addherbs@gmail.com>
2017-09-16 14:05:20 -0500

    Changes in users.js, added path for createPortal

--- routes/users.js
+++ routes/users.js
@@ -2,18 +2,87 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-
 var User = require('../models/users');
+
 
 // register route
 router.get('/register', function (req,res) {
     res.render('register');
 });
 
-//login route
+
+// login route
 router.get('/login', function (req,res) {
     res.render('login');
 });
+
+
+// verify which button is pressed
+router.post('/twoButton', function (req,res) {
+
+        req.body = JSON.parse(JSON.stringify(req.body));
+        if(req.body.hasOwnProperty('createPortal')){
+            console.log('create portal was clicked!!');
+            res.render('createPortal');
+        }else{
+            console.log('Show portal was clicked!!');
+            res.render('index');
+        }
+});
+
+
+// Create a portal
+router.post('/createPortal', function (req,res) {
+    var pName = req.body.pName;
+    var pPassword = req.body.pPassword;
+    var hours = req.body.hours;
+    var mins = req.body.mins;
+    var secs = req.body.secs;
+    var message = req.body.message;
+    var ttl = hours*3600 + mins*60 + secs;
+    console.log("Portal Name:"+ pName + "\nPortal Password:"+pPassword +"\nHours-Mins-Secs: "+ hours +"-" +mins+ "-"+ secs + "\nMessage:" + message + "\nTotal TTL: "+ ttl);
+
+
+    //Validation of form
+    req.checkBody('pName', 'Portal Name is required').notEmpty();
+    req.checkBody('pPassword', 'Portal Password is required').notEmpty();
+    req.checkBody('hours', 'Hours is required/ Otherwise enter 0').notEmpty();
+    req.checkBody('mins', 'Minutes is required/ Otherwise enter 0').notEmpty();
+    req.checkBody('secs', 'Seconds is required/ Otherwise enter 0').notEmpty();
+    req.checkBody('message', 'There has to be a message').notEmpty();
+
+
+
+    var errors= req.validationErrors();
+    if(errors){
+        res.render('createPortal',{
+            errors:errors
+        });
+    }else{
+        var newPortal = new User({
+            FirstName: name,
+            UserName: username,
+            EmailID: email,
+            Password: password
+        });
+
+        //
+        // User.createUser(newPortal, function(err, user){
+        //     if (err) throw err;
+        //     console.log(user);
+        // });
+
+        req.flash('success_msg', 'You are now registered.. Login to continue');
+
+        res.redirect('/users/login');
+    }
+
+
+
+
+    res.render('index');
+});
+
 
 router.post('/register', function (req,res) {
 
@@ -104,9 +173,5 @@ router.get('/logout', function (req,res) {
 
    res.redirect('/users/login');
 });
-
-
-
-
 
 module.exports = router;
