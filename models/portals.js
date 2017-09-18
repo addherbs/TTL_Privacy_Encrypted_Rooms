4558c408c51187b9ca783a4f7870a98c2a223233
Aditya Pandey <addherbs@gmail.com>
2017-09-18 04:13:13 -0500

    changes

--- models/portals.js
+++ models/portals.js
@@ -1,19 +1,49 @@
 var mongoose = require('mongoose');
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
 
-var PortalSchema= mongoose.Schema({
+var PortalSchema = mongoose.Schema({
 
-    UserName:{
+    PortalName:{
         type:String
-    },
-    PortalName: {
-        type: String,
         index: true
     },
     PortalPassword:{
         type:String
     },
-    EmailID:{
-        type:String
-    }
+    TTL:{
+        type:Number
+    },
+    Message: {
+        type: String,
 
+    }
 });
+
+var Portal = module.exports = mongoose.model('Portal', PortalSchema);
+
+
+module.exports.createPortal= function(newPortal,callback){
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(newPortal.pPassword, salt, function(err, hash) {
+
+            newPortal.pPassword = hash;
+
+            db.collection('portals').insertOne({
+
+                PortalName: newPortal.pName,
+                PortalPassword: newPortal.pPassword,
+                TTL: newPortal.ttl,
+                Message: newPortal.message
+
+            }, function (err, result) {
+                assert.equal(err, null);
+                console.log("Inserted a document into the restaurants collection.");
+                callback();
+            });
+
+            // newPortal.save(callback);
+
+        });
+    });
+};
