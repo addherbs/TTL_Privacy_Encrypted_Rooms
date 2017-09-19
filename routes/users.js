ee8d2cf6f35fcd9e7d721475c116ad6a0a3ccc25
Aditya Pandey <addherbs@gmail.com>
2017-09-19 17:24:43 -0500

    final users.js for listing the portal

--- routes/users.js
+++ routes/users.js
@@ -27,20 +27,17 @@ router.post('/twoButton', function (req,res) {
             res.render('createPortal');
         }else{
             console.log('Show portal was clicked!!');
-            var listOfAllPortals;
+
                 Portal.getPortals(function(err, portals){
                 if (err) throw err;
+                var listOfAllPortals = portals;
 
-                listOfAllPortals = portals;
-
+                console.log("Check starts ------------------");
+                console.log(listOfAllPortals);
+                console.log("Check Ends ------------------");
+                res.render('showPortals', {portals:listOfAllPortals});
             });
 
-            console.log("Check starts ------------------");
-            console.log(listOfAllPortals);
-            console.log("Check Ends ------------------");
-
-
-            res.render('index');
         }
 });
 
