fab4ac951eea24c6616a5cca35562f885f3c17a8
Aditya Pandey <addherbs@gmail.com>
2017-11-19 09:08:17 -0600

    Updated Model for portal
    Added a new entry Owner_ID
    This is for the one who created the portal
    
    Working successfully

--- models/portals.js
+++ models/portals.js
@@ -23,13 +23,15 @@ var PortalSchema = mongoose.Schema({
     },
     Count: {
         type: Number
+    },
+    Owner_ID: {
+        type: String
     }
 });
 
 var Portal = module.exports = mongoose.model('Portal', PortalSchema);
 
-
-module.exports.createPortal= function(newPortal,callback){
+module.exports.createPortal= function(newPortal,callback) {
     bcrypt.genSalt(10, function(err, salt) {
         bcrypt.hash(newPortal.PortalPassword, salt, function(err, hash) {
 
@@ -38,12 +40,11 @@ module.exports.createPortal= function(newPortal,callback){
             db.collection('portals').insertOne(
                 newPortal       //This is the object, we can also insert in a jason format..
                 , function (err, result) {
-                assert.equal(err, null);
-                console.log("Inserted a document into the Portals collection.");
-                callback();
-            });
+                    assert.equal(err, null);
+                    console.log("Inserted a document into the Portals collection.");
+                    callback();
+                });
             // newPortal.save(callback);
-
         });
     });
 };
@@ -53,11 +54,40 @@ module.exports.getPortals = function (callback) {
     //mongoose get all docs. I think here answers your question directly
     Portal.find(function (err, results) {
         if (err) {
-            console.log("An error has occured. Abort everything!");
+            console.log("An error has occurred. Abort everything!");
             callback(err);
         }
         assert.equal(null, err);
 
         callback(null, results);
+    });
+};
+
+
+// module.exports.getPortalByID = function (id,callback) {
+//     //Get a portal with a specific id, if not, error is returned
+//     db.collection('portals').find(
+//         id //This is the object in json format..
+//         , function (err, result) {
+//             assert.equal(err, null);
+//             console.log("Got the document");
+//             console.log(result);
+//             callback(result);
+//         });
+// };
+
+module.exports.getPortalByID = function (id,callback) {
+
+    db.collection('portals', function (err, collection) {
+        if (!err) {
+            collection.find(
+                id
+            ).toArray(function(err, docs) {
+                if (!err) {
+                    console.log("No error while retrieving ");
+                    console.log(docs);
+                    callback(docs);
+                }});
+        }
     });
 };
